c36a1091b184c1b52033f71a85cbfe4a06f4efbf
Mhayk Whandson <hi@mhayk.com>
2020-06-04 00:02:55 +0100

    The map has been added in the form.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
+import { Map, TileLayer, Marker } from 'react-leaflet'
 
 
 import './styles.css';
@@ -50,6 +51,15 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+
+                    <Map center={[51.4619707,-0.358245]} zoom={14}>
+                    <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+
+                    <Marker position={[51.4619707,-0.358245]} />
+                    </Map>
 
                     <div className="field-group">
                         <div className="field">
